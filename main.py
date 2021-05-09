885e55b7147d840128823c628bd06dd4c3069baa
Chessum <chrissetterst@gmail.com>
2021-05-09 17:50:53 -0400

    Create basic main file.

---
+++ main.py
@@ -0,0 +1,9 @@
+# TODO Define game board
+
+# TODO Allow choice between human vs human or human vs AI
+
+# TODO Take user input to change game board
+
+# TODO Determine end-game conditions
+
+# TODO Let player opt to exit or rematch.
