d77e7d82b2d9f0ab1268de89d6563574d2afb7d1
Chessum <chrissetterst@gmail.com>
2021-05-09 18:44:30 -0400

    Program sketch

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
-# TODO Define game board
+# TODO Function - take player input
+def takePlayerInput():
+    print("Select column: ")
+    col = input()
 
-# TODO Allow choice between human vs human or human vs AI
+    print("Select row: ")
+    row = input()
+    return col, row
 
-# TODO Take user input to change game board
+def checkGameOver():
+    print("reached checkGameOver")
+    return True
+
+
+# Define game board
+
+Board = [   ['.', '.', '.'],
+            ['.', '.', '.'],
+            ['.', '.', '.']]
+
+# Allow shape choice for player one.
+
+print("What shape is player 1?\n")
+playerChoice = input()
+playerOneShape = playerChoice
+
+print("What shape is player 2?\n")
+playerChoice = input()
+playerTwoShape = playerChoice
+
+# TODO Run game round loop that goes until end-game conditions are met.
+isGameOver = False
+playerOneTurn = True
+
+while (isGameOver is False):
+
+    if playerOneTurn is True:
+        print("Player 1")
+        col, row = takePlayerInput()
+        Board[col][row] = playerOneShape
+    elif playerOneTurn is False:
+        print("Player 1")
+        col, row = takePlayerInput()
+        Board[col][row] = playerTwoShape
+    else:
+        print ("Error bA31")
+
+    isGameOver = checkGameOver()
+
+print("Game is over. Goodbye")
+
+# Take user input to change game board state
+# Run player input function assigning alternately to player one or two depending on the turn.
+
+
 
 # TODO Determine end-game conditions
 
